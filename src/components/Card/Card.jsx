bda536ac2253e098253efb38ec0c9478c97250e5
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-10-25 22:04:38 -0300

    feat: adding favorites for favorites characters

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { LinearProgress, Container } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { MdOutlineFavoriteBorder } from 'react-icons/md';
+import { AiFillEdit } from "react-icons/ai";
 import { BiTrashAlt } from 'react-icons/bi';
+import { HeroContext } from '../../context/hero.context';
 import './index.css';
 
-const Card = ({ appearance, biography, connections, id, name, images, powerstats, slug, work }) => {
+const Card = ({ appearance, biography, connections, id, name, images, powerstats, slug, work, apelido }) => {
 
     const [favorite, setFavorite] = useState(false);
     const [deleted, setDeleted] = useState(false);
     const [powerStatsArray, setPowerStatsArray] = useState([]);
+    const [edit, setEdit] = useState(false);
+    const [newSurname, setNewSurname] = useState("");
+    const { setFavorites, favorites } = useContext(HeroContext);
+    let currentFavorite = {}
 
     useEffect(() => {
         handlePoweStats();
-    }, [id])
+    }, [])
 
     const theme = createTheme({
         palette: {
@@ -42,6 +48,42 @@ const Card = ({ appearance, biography, connections, id, name, images, powerstats
         setPowerStatsArray(AuxPowerStatsArray);
     }
 
+    const handleAddingFavorite = () => {
+        formatFavorite();
+        setFavorites([...favorites, currentFavorite]);
+        setFavorite(!favorite)
+        currentFavorite = {}
+    }
+
+    const handleSearchingFavorite = (e) => {
+        e.preventDefault();
+        setEdit(true);
+        let current = favorites.find((favorite) => favorite.id === id);
+        let new_favorites = favorites.filter((favorite) => favorite.id !== id);
+        current.apelido = newSurname;
+        new_favorites.push(current);
+        setFavorites(new_favorites);
+        setEdit(false);
+        setNewSurname("");
+    }
+
+    const handleDeletingFavorite = () => {
+        let new_favorites = favorites.filter((favorite) => favorite.id !== id);
+        setFavorites(new_favorites);
+    }
+
+    const formatFavorite = () => {
+        currentFavorite.appearance = appearance;
+        currentFavorite.biography = biography;
+        currentFavorite.connections = connections;
+        currentFavorite.id = id;
+        currentFavorite.name = name;
+        currentFavorite.images = images;
+        currentFavorite.powerstats = powerstats;
+        currentFavorite.slug = slug;
+        currentFavorite.work = work
+    }
+
     return (
         <div className="card-container">
             <div className='card-data'>
@@ -50,7 +92,14 @@ const Card = ({ appearance, biography, connections, id, name, images, powerstats
                         <img src={images.md} alt="Super Hero" className='image-super-hero' />
                     </div>
                     <div className="data-super-hero">
-                        <p className='name-super-hero'>{name}</p>
+                        <p className='name-super-hero'>{name}
+                            {apelido ?
+                                <p className="surname-super-hero"> ({apelido})</p>
+                                : edit && <form onSubmit={(e) => handleSearchingFavorite(e)}>
+                                    <input type="text" placeholder="Adding Surname ..." className="edit-surname" onChange={(event) => setNewSurname(event.target.value)} value={newSurname} />
+                                </form>
+                            }
+                        </p>
                         <div className='attributes-super-hero'>
                             <span>Gender</span>
                             <span>{appearance.gender}</span>
@@ -70,11 +119,11 @@ const Card = ({ appearance, biography, connections, id, name, images, powerstats
                         {
                             powerStatsArray.map((data, index) => {
                                 return (
-                                    <div className='attributes-super-hero'>
+                                    <div className='attributes-super-hero' key={index}>
                                         <span>{data.attribute}</span>
                                         <Container className='text-center'>
                                             <ThemeProvider theme={theme}>
-                                                <LinearProgress variant="determinate" value={data.power} color="primary" style={{paddingRight: '0px !important'}}/>
+                                                <LinearProgress variant="determinate" value={data.power} color="primary" style={{ paddingRight: '0px !important' }} />
                                             </ThemeProvider>
                                         </Container>
                                     </div>
@@ -82,8 +131,9 @@ const Card = ({ appearance, biography, connections, id, name, images, powerstats
                             })
                         }
                         <div className='buttons-super-hero'>
-                            <BiTrashAlt color={favorite ? "red" : "#fff"} size={25} onClick={() => setFavorite(!favorite)} />
-                            <MdOutlineFavoriteBorder color={deleted ? "red" : "#fff"} size={25} onClick={() => setDeleted(!deleted)} />
+                            <BiTrashAlt color={deleted ? "red" : "#fff"} size={25} onClick={() => handleDeletingFavorite()} />
+                            <MdOutlineFavoriteBorder color={favorite ? "red" : "#fff"} size={25} onClick={() => handleAddingFavorite(name)} />
+                            {window.location.href.includes("favorites") && <AiFillEdit size={25} onClick={() => setEdit(true)} />}
                         </div>
                     </div>
                 </section>
