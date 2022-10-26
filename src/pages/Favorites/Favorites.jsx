e4958d671055d019b206c120ec007a1078e54e26
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-10-25 21:16:18 -0300

    feat: adding favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import Card from '../../components/Card';
+import { HeroContext } from '../../context/hero.context';
+import "./index.css";
 
 const Favorites = () => {
 
-    return (
+    const { favorites } = useContext(HeroContext);
 
-        <div className="favorites-container">
-            <h1>Favorites</h1>
-        </div>
-    )
+    if (favorites.length > 0) {
+        return (
+            <div className="pt-5">
+                <div className="row">
+                    <h1>Your favorites characters</h1>
+                    {
+                        favorites.map((favorite, index) => {
+                            return (
+                                <div className="col-md-3 col-12 card-list p-0 m-0" key={index}>
+                                    <Card
+                                        appearance={favorite.appearance}
+                                        biography={favorite.biography}
+                                        connections={favorite.connections}
+                                        id={favorite.id}
+                                        name={favorite.name}
+                                        images={favorite.images}
+                                        powerstats={favorite.powerstats}
+                                        slug={favorite.slug}
+                                        work={favorite.work}
+                                        apelido={favorite.apelido && favorite.apelido}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="container pt-5">
+                <div className="container-not-favorites">
+                    <h1>You still don't have favorite characters</h1>
+                </div>
+                {console.log("-----", favorites)}
+            </div>
+        )
+    }
 }
 
 export default Favorites;
